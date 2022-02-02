31839a9c9d3c8c42fc2daf3b16e07f2ff1a9838e
Ledwos <32643853+Ledwos@users.noreply.github.com>
2022-02-02 21:48:08 +0000

    multiple markers

--- front/src/Marker.js
+++ front/src/Marker.js
@@ -1,47 +1,42 @@
-import React, { createContext } from 'react';
-import { ItemTypes } from './items';
-import { useDrag } from 'react-dnd'
+import React, { createContext } from "react";
+import { ItemTypes } from "./items";
+import { useDrag } from "react-dnd";
 
 export const markerContext = createContext({
-    markerUpper: true
-})
+  markerUpper: true,
+});
 
-const Marker = ({coord}) => {
-    const [{isDragging}, drag] = useDrag({
-        type: ItemTypes.MARKER,
-        item: {
-            id: "test",
-            test: "test"
-        },
-        collect: monitor => ({
-            isDragging: !!monitor.isDragging()
-        }),
-    });
-    const markerUpper = () => {
-        
-    }
-    return (
-        <markerContext.Provider value={markerUpper}>
-            <div
-            ref={drag}
-            style={{
-                borderRadius: "50%",
-                backgroundColor: "teal",
-                width: "30px",
-                height: "30px",
-                textAlign: "center",
-                color: "white",
-                opacity: (isDragging ? "0" : "1"),
-                position: 'absolute',
-                left: `${coord.x}px`,
-                top: `${coord.y}px`,
-            }}
-            id="test"
-            >
-                :D
-            </div>
-        </markerContext.Provider>
-    );
+const Marker = ({ coord }) => {
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.MARKER,
+    item: coord,
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+  const markerUpper = () => {};
+  return (
+    <markerContext.Provider value={markerUpper}>
+      <div
+        ref={drag}
+        style={{
+          borderRadius: "50%",
+          backgroundColor: "teal",
+          width: "30px",
+          height: "30px",
+          textAlign: "center",
+          color: "white",
+          position: "absolute",
+          opacity: isDragging ? "0" : "1",
+          left: `${coord.x}px`,
+          top: `${coord.y}px`,
+        }}
+        class="marker"
+      >
+        :D
+      </div>
+    </markerContext.Provider>
+  );
 };
 
 export default Marker;
--- front/src/Upper/Upper.js
+++ front/src/Upper/Upper.js
@@ -1,33 +1,49 @@
-import React, { useState } from 'react';
-import Marker from '../Marker'
-import { useDrop } from 'react-dnd';
-import { ItemTypes } from '../items';
+import React, { useState } from "react";
+import Marker from "../Marker";
+import { useDrop } from "react-dnd";
+import { ItemTypes } from "../items";
 
 const Upper = () => {
-    const [markerJson, setMarkerJson] = useState({
-        x: 10, y: 10
-    })
-    const [, drop] = useDrop({ 
-        accept: ItemTypes.MARKER,
-        drop: (item, monitor) => {
-            const delta = monitor.getDifferenceFromInitialOffset();
-            // console.log(delta.x, delta.y)
-            const newMarkerJson = {
-                x: delta.x + markerJson.x,
-                y: delta.y + markerJson.y
-            }
-            console.log(newMarkerJson.x, newMarkerJson.y)
-            setMarkerJson(newMarkerJson)
-        },
-    })
-    return (
-        <div id="upper"
-        ref={drop}
-        >
-            markers start here
-            <Marker coord={markerJson}/>
-        </div>
-    );
+  const [markerJson, setMarkerJson] = useState([
+    {
+      id: "m1",
+      x: 300,
+      y: 50,
+    },
+    {
+      id: "m2",
+      x: 213,
+      y: 10,
+    },
+    {
+      id: "m3",
+      x: 580,
+      y: 150,
+    },
+  ]);
+  const [, drop] = useDrop({
+    accept: ItemTypes.MARKER,
+    drop: (item, monitor) => {
+      const delta = monitor.getDifferenceFromInitialOffset();
+      const newJson = {
+        id: item.id,
+        x: delta.x + item.x,
+        y: delta.y + item.y,
+      };
+      const newMarkerJson = [...markerJson];
+      const markerIdx = newMarkerJson.findIndex((x) => x.id === item.id);
+      newMarkerJson[markerIdx] = newJson;
+      setMarkerJson(newMarkerJson);
+    },
+  });
+  return (
+    <div id="upper" ref={drop}>
+      markers start here
+      {markerJson.map((marker, idx) => (
+        <Marker coord={marker} key={idx} />
+      ))}
+    </div>
+  );
 };
 
 export default Upper;
